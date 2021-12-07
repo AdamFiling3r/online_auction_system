c54dcf77005f99567c03ba19a057f35f6c80e135
SlutDragon <martin.chmelar@student.gyarab.cz>
2021-12-07 12:37:25 +0100

    Něco

--- connect.php
+++ connect.php
@@ -1,5 +1,12 @@
 <_php
-$user = "db-308";
-$password = "6ug2rvoi";
-$db = "";
-$host = 
+
+$conn=ssh2_connect('db.gyarab.cz',22);
+ssh2_auth_password($conn,'dk-313','GyArab14');
+
+$tunnel = ssh2_tunnel($conn, 'localhost', 3306);
+$mysqli=new mysqli('localhost', 'dk-313', 'GyArab14', 'dk-313', 3306, $tunnel);
+$SQL=;
+$result=$mysqli->query('select * from adresar');
+
+
+
