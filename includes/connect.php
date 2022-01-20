fb517c8d5f623124f10591c115b28a4d581c1ecf
adam-filinger <adam.filinger@gmail.com>
2022-01-20 16:32:40 +0100

    old hosting

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,8 @@
 <?php
-    $user = "epiz_30862495";
-    $password_db = "KahounNefunguje123";
-    $db = "epiz_30862495_rocnikovka_aukce";
-    $host = "sql301.epizy.com";
+    $user = "sql4457624";
+    $password_db = "UBNemP9Mjk";
+    $db = "sql4457624";
+    $host = "sql4.freemysqlhosting.net";
 
      $conn = mysqli_connect($host, $user, $password_db, $db);
      if(!$conn){
