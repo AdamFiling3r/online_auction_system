4f9a06237ffc0f6628ff1f89bf63bbd1362490b2
SlutDragon <martin.chmelar@student.gyarab.cz>
2022-04-05 23:05:31 +0200

    miluju Adama

--- auction.php
+++ auction.php
@@ -14,6 +14,10 @@ $auction = searchArray(2, $_GET["offer_id"]);
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
     <title>Auction</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
 </head>
 
 <body class="bg-dark">
